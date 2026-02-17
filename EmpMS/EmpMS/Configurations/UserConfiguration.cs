cdb0b35956eac2d36d2dab7613bd581a2040b70f
KartikZCoding <ahir.kartik.385@gmail.com>
2026-02-17 13:50:57 +0530

    (Table) :- create configurations for Auth tables

---
+++ EmpMS/EmpMS/Configurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using EmpMS.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmpMS.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+            builder.HasIndex(u => u.Username).IsUnique();
+            builder.HasIndex(u => u.Email).IsUnique();
+
+            builder.Property(u => u.Id).ValueGeneratedOnAdd();
+
+            builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.PasswordHash).IsRequired();
+            builder.Property(u => u.IsActive).HasDefaultValue(true);
+            builder.Property(u => u.CreatedAt).HasDefaultValueSql("GETDATE()");
+
+        }
+    }
+}
