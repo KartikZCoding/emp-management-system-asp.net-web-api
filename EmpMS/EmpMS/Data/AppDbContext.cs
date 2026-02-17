276b942e15f44b65e92b271c5f650a6f3593e5e6
KartikZCoding <ahir.kartik.385@gmail.com>
2026-02-17 13:56:53 +0530

    (Migration) :- manage DBContext as per tables and Add migration to Database

--- EmpMS/EmpMS/Data/AppDbContext.cs
+++ EmpMS/EmpMS/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmpMS.Configurations;
+using EmpMS.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmpMS.Data
 {
@@ -11,7 +13,20 @@ namespace EmpMS.Data
         }
 
         //Dbsets (Tables)
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Privilege> Privileges { get; set; }
+        public DbSet<RolePrivilege> RolePrivileges { get; set; }
 
         //Configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new PrivilegeConfiguration());
+            modelBuilder.ApplyConfiguration(new RolePrivilegeConfiguration());
+        }
     }
 }
