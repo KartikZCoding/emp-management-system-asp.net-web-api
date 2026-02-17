742cfabd923e7309b1843cbdb33f0c1fac88d3a1
KartikZCoding <ahir.kartik.385@gmail.com>
2026-02-17 12:36:22 +0530

    (DB-Entities) :- add Entity for authentication modules.

---
+++ EmpMS/EmpMS/Models/RolePrivilege.cs
@@ -0,0 +1,14 @@
+﻿namespace EmpMS.Models
+{
+    public class RolePrivilege
+    {
+        //table prop
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public int PrivilegeId { get; set; }
+
+        //navigation prop
+        public Role Role { get; set; }
+        public Privilege Privilege { get; set; }
+    }
+}
