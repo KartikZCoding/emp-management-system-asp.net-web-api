2b9d0515e9021c49abe7abc0ecddfc660616a6df
KartikZCoding <ahir.kartik.385@gmail.com>
2026-02-17 12:16:04 +0530

    (DB) :- Add database with it's context and registered in program.cs

---
+++ EmpMS/EmpMS/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpMS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //constructor for DB Context
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+           
+        }
+
+        //Dbsets (Tables)
+
+        //Configurations
+    }
+}
