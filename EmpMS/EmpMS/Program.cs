829257f41cf785379ccd20ac347cdcd6ae9fb8bc
KartikZCoding <ahir.kartik.385@gmail.com>
2026-03-13 17:42:09 +0530

    (deployment) :- edited for testing

--- EmpMS/EmpMS/Program.cs
+++ EmpMS/EmpMS/Program.cs
@@ -145,11 +145,20 @@ app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwagger(); app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvMS v1");
+
+    c.RoutePrefix = string.Empty;
+
+});
 
 app.UseHttpsRedirection();
 
