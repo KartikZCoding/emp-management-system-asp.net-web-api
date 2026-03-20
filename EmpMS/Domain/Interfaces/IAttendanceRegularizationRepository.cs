317b2b968f8ce2c418fd04365e15d2c26c5c3589
KartikZCoding <ahir.kartik.385@gmail.com>
2026-03-20 21:59:21 +0530

    (Repo) :- adding a repo of that with CRUD

---
+++ EmpMS/Domain/Interfaces/IAttendanceRegularizationRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public  interface IAttendanceRegularizationRepository
+    {
+        Task<AttendanceRegularization?> GetByIdAsync(int id);
+        Task<List<AttendanceRegularization>> GetByEmployeeIdAsync(int employeeId);
+        Task<List<AttendanceRegularization>> GetPendingAsync();
+        Task CreateAsync(AttendanceRegularization request);
+        Task UpdateAsync(AttendanceRegularization request);
+        Task<bool> HasPendingRequestAsync(int attendanceId);
+
+    }
+}
---
+++ EmpMS/Infrastructure/Repositories/AttendanceRegularizationRepository.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class AttendanceRegularizationRepository : IAttendanceRegularizationRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AttendanceRegularizationRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<AttendanceRegularization?> GetByIdAsync(int id)
+        {
+            return await _appDbContext.AttendanceRegularizations
+                .Include(e => e.Employee)
+                .Include(e => e.Attendance.AttendanceLogs)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<List<AttendanceRegularization>> GetByEmployeeIdAsync(int employeeId)
+        {
+            return await _appDbContext.AttendanceRegularizations
+                .Include(e => e.Employee)
+                .Include(e => e.Attendance.AttendanceLogs)
+                .Where(e => e.EmployeeId == employeeId).OrderByDescending(e => e.CreatedAt).ToListAsync();
+        }
+
+        public async Task<List<AttendanceRegularization>> GetPendingAsync()
+        {
+            return await _appDbContext.AttendanceRegularizations
+                .Include(e => e.Employee)
+                .Where(e => e.Status == "Pending").ToListAsync();
+        }
+
+        public async Task CreateAsync(AttendanceRegularization request)
+        {
+            await _appDbContext.AttendanceRegularizations.AddAsync(request);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(AttendanceRegularization request)
+        {
+            _appDbContext.AttendanceRegularizations.Update(request);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> HasPendingRequestAsync(int attendanceId)
+        {
+            return await _appDbContext.AttendanceRegularizations.AnyAsync(e => e.AttendanceId == attendanceId && e.Status == "Pending");
+        }
+    }
+}
