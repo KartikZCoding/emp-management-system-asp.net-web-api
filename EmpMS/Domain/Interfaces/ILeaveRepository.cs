6025175b7ca7f315e17df6117f0bede233f52dc3
KartikZCoding <ahir.kartik.385@gmail.com>
2026-03-23 22:09:59 +0530

    (Bussiness Rule defined) :-adding a repo interface in domain

---
+++ EmpMS/Domain/Interfaces/ILeaveRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface ILeaveRepository
+    {
+        /*Leave Type*/
+        Task<List<LeaveType>> GetAllLeaveTypesAsync();
+        Task<LeaveType?> GetLeaveTypeByIdAsync(int id);
+        Task CreateLeaveTypeAsync(LeaveType leaveType);
+        Task UpdateLeaveTypeAsync(LeaveType leaveType);
+        Task<bool> LeaveTypeExistsAsync(string name);
+
+        /*Leave Balance*/
+        Task<List<LeaveBalance>> GetBalancesByEmployeeAsync(int employeeId, int year);
+        Task<LeaveBalance?> GetBalanceAsync(int employeeId, int leaveTypeId, int year);
+        Task CreateBalanceAsync(LeaveBalance leaveBalance);
+        Task UpdateBalanceAsync(LeaveBalance leaveBalance);
+        Task AssignBalancesForEmployeeAsync(int employeeId, int year);
+
+        /*Leave Request*/
+        Task<LeaveRequest?> GetRequestByIdAsync(int id);
+        Task<List<LeaveRequest>> GetRequestsByEmployeeAsync(int employeeId);
+        Task<List<LeaveRequest>> GetPendingRequestAsync();
+        Task CreateRequestAsync(LeaveRequest leaveRequest);
+        Task UpdateRequestAsync(LeaveRequest leaveRequest);
+        Task<bool> HasOverlappingRequestAsync(int employeeId, DateOnly start, DateOnly end);
+    }
+}
