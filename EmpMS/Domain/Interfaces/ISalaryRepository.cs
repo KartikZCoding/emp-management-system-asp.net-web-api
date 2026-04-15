735ef75eff4680ad159b04972825da4f671fc64a
KartikZCoding <ahir.kartik.385@gmail.com>
2026-04-15 17:51:10 +0530

    (Repo) :- Adding repository interface for this module 7 salary and payroll

---
+++ EmpMS/Domain/Interfaces/ISalaryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface ISalaryRepository
+    {
+        Task<Salary?> GetByIdAsync(int id);
+        Task<Salary?> GetByEmployeeMonthYearAsync(int employeeId, int month, int year);
+        Task<List<Salary>> GetByEmployeeAsync(int employeeId, int? month, int? year);
+        Task<List<Salary>> GetAllByMonthYearAsync(int month, int year);
+        Task<List<Salary>> GetYearlyAllAsync(int year);
+        Task<bool> ExistsAsync(int employeeId, int month, int year);
+        Task AddAsync(Salary salary);
+        Task AddRangeAsync(List<Salary> salaries);
+        void Update(Salary salary);
+    }
+}
